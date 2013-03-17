865bf7b46eff53d66b81651f4a80479c333c3905
Sam Fries <samuelf@4thdownsoftware.com>
2013-03-16 20:22:04 -0700

    Added a ConsumeBytes method to StateMachine, added .DS_Store to .gitignore

--- state.go
+++ state.go
@@ -1,21 +1,31 @@
 package state
 
 type StateMachine struct {
-	States       []func(byte) (string, int)
+	States       []func(byte) (string, StateChange)
 	Output       []string
 	CurrentState int
 }
 
 func (machine *StateMachine) AddState(state func(byte) (string, int)) {
-    
-    machine.States = append(machine.States, state)
+
+	machine.States = append(machine.States, state)
 
 }
 
-func (machine *StateMachine) Consume(data byte) {
+func (machine *StateMachine) ConsumeByte(data byte) {
 
 	var result string
-	result, machine.CurrentState = machine.States[machine.CurrentState](data)
-	machine.Output = append(machine.Output, result)
 
+	result, machine.CurrentState = machine.States[machine.CurrentState](data)
+
+	if result != "" {
+		machine.Output = append(machine.Output, result)
+	}
+
+}
+
+func (machine *StateMachine) ConsumeBytes(data []byte) {
+	for index, data_byte := range data {
+		machine.ConsumeByte(data_byte)
+	}
 }
